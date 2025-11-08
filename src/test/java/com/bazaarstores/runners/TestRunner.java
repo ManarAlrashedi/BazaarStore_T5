f2e793cceaf7ed32f83923203a53fba3b8bb2bd4
Glohaya <alzuairhaya@gmail.com>
2025-11-08 21:37:10 +0300

    Customer Confirm items in the cart

--- src/test/java/com/bazaarstores/runners/TestRunner.java
+++ src/test/java/com/bazaarstores/runners/TestRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/bazaarstores/stepDefinitions",
-        tags = "@Customer",
+        tags = "@CustomerCart",
         dryRun = false
 )
 public class TestRunner {
