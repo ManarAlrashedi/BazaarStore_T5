808b6452e2a533a286e57551736d0641b044fcc7
ManarAlrashedi <mn.alrashedi5@gmail.com>
2025-11-17 14:36:04 +0300

    Refactor

--- src/test/java/com/bazaarstores/runners/TestRunner.java
+++ src/test/java/com/bazaarstores/runners/TestRunner.java
@@ -14,6 +14,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/bazaarstores/stepDefinitions",
+        tags = "@Smoke",
         dryRun = false
 )
 public class TestRunner {
